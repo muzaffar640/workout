7885136390fdd2a4775334aa79ba83aba1b2b738
Muzaffar Hossain <muzaffar640@gmail.com>
2023-05-30 20:45:28 +0530

    Removed unused index.js route file

--- src/v1/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.route("/").get((req, res) => {
-  res.send(`<h3> Hey there from ${req.baseUrl}</h3>`);
-});
-
-module.exports = router;
