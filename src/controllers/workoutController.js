ef8078094d07c474a8a276101c2099c17b281522
Muzaffar Hossain <muzaffar640@gmail.com>
2023-05-31 09:23:35 +0530

    Populate workout service layer and connect to controller

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+const workoutService = require("../services/workoutServices");
+
 const getAllWorkouts = (req, res) => {
+  const allWorkouts = workoutService.getAllWorkouts();
   res.send("Get all workouts");
 };
 
 const getOneWorkout = (req, res) => {
+  const oneWorkout = workoutService.getOneWorkout();
   res.send("Get an existing workout");
 };
 
 const createOneWorkout = (req, res) => {
+  const createWorkout = workoutService.createOneWorkout();
   res.send("Create one workout");
 };
 
 const updateOneWorkout = (req, res) => {
+  const updateWorkout = workoutService.updateOneWorkout();
   res.send("Update an workout");
 };
 
 const deleteOneWorkout = (req, res) => {
+  const deleteWorkout = workoutService.deleteOneWorkout();
   res.send("Delete one workouts");
 };
 
---
+++ src/services/workoutServices.js
@@ -0,0 +1,24 @@
+const getAllServices = () => {
+  return;
+};
+
+const getOneService = () => {
+  return;
+};
+const createOneService = () => {
+  return;
+};
+const updateOneService = () => {
+  return;
+};
+const deleteOneService = () => {
+  return;
+};
+
+module.export = {
+  getAllServices,
+  getOneService,
+  createOneService,
+  updateOneService,
+  deleteOneService,
+};
