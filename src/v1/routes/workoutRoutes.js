03b6e72284e52048f8be0e64e5ab7733ea1924c0
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-03 12:49:46 +0530

    Cache the getAllWorkout api

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
+const apiCachec = require("apicache");
 
 const router = express.Router();
+const cache = apiCachec.middleware;
 
-router.get("/", workoutController.getAllWorkouts);
+//for now I have cache the getAllWorkout api we can do it for all the apis on index.js file
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
 router.get("/:workoutId/records", recordController.getRecordsForWorkout);
